b7ddabb5aee6072a4c1a0b1165acabcc97e87834
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-06-30 12:55:28 +0200

    T5: Allow clear text network communication for debug and devs.

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.koin.android)
 
+    implementation(libs.compose.material3)
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
 }

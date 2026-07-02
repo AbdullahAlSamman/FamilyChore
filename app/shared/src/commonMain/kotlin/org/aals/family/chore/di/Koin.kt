99ad5f09843df8d373cd0c707b545dcb6fa5840e
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-07-02 14:56:34 +0200

    T11: Consolidation of logging initialization into shared Koin setup and implementation of Koin initialization for iOS.

--- app/shared/src/commonMain/kotlin/org/aals/family/chore/di/Koin.kt
+++ app/shared/src/commonMain/kotlin/org/aals/family/chore/di/Koin.kt
@@ -1,11 +1,13 @@
 package org.aals.family.chore.di
 
 import org.aals.family.chore.core.di.coreModule
+import org.aals.family.chore.core.domain.util.LoggingInitializer
 import org.aals.family.chore.feature.auth.di.authModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
 fun initKoin(config: KoinAppDeclaration? = null) {
+    LoggingInitializer.init()
     startKoin {
         config?.invoke(this)
         modules(coreModule, authModule)

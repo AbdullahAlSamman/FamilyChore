eac38f3d623d94c8f7b308ab8876c2639a876d18
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-09-06 19:04:12 +0200

    T21: Conditional configuration of iOS targets and Room KSP compilers based on host operating system in KMP library convention plugin and core module.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -52,11 +52,4 @@ kotlin {
     }
 }
 
-dependencies {
-    // Room KSP for all targets
-    val roomCompiler = libs.room.compiler
-    add("kspAndroid", roomCompiler)
-    add("kspJvm", roomCompiler)
-    add("kspIosArm64", roomCompiler)
-    add("kspIosSimulatorArm64", roomCompiler)
-}
+
